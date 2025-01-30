1baa61ca4b388d1dd3f2a8071d8c8785d61487a9
Maria Desteffani <maria.desteffani@unibas.ch>
2025-01-30 17:42:16 +0100

    updated lab experiment

--- experiments/30-01-24-pipeline_test.py
+++ experiments/30-01-24-pipeline_test.py
@@ -31,13 +31,18 @@ SCRIPT_DIR = Path(__file__).parent
 REPO_DIR = SCRIPT_DIR.parent
 BENCHMARKS_DIR = Path(os.environ["DOWNWARD_BENCHMARKS"])
 
-TIME_LIMIT = 1800
+TIME_LIMIT = 300    
 MEMORY_LIMIT = 3584
+
+# benchmarks
+optimal_strips = ["agricola-opt18-strips", "airport", "barman-opt11-strips", "barman-opt14-strips", "blocks", "childsnack-opt14-strips", "data-network-opt18-strips", "depot", "driverlog", "elevators-opt08-strips", "elevators-opt11-strips", "floortile-opt11-strips", "floortile-opt14-strips", "freecell", "ged-opt14-strips", "grid", "gripper", "hiking-opt14-strips", "logistics00", "logistics98", "miconic", "movie", "mprime", "mystery", "nomystery-opt11-strips", "openstacks-opt08-strips", "openstacks-opt11-strips", "openstacks-opt14-strips", "openstacks-strips", "organic-synthesis-opt18-strips", "organic-synthesis-split-opt18-strips", "parcprinter-08-strips", "parcprinter-opt11-strips", "parking-opt11-strips", "parking-opt14-strips", "pathways", "pegsol-08-strips", "pegsol-opt11-strips", "petri-net-alignment-opt18-strips", "pipesworld-notankage", "pipesworld-tankage", "psr-small", "quantum-layout-opt23-strips", "rovers", "satellite", "scanalyzer-08-strips", "scanalyzer-opt11-strips", "snake-opt18-strips", "sokoban-opt08-strips", "sokoban-opt11-strips", "spider-opt18-strips", "storage", "termes-opt18-strips", "tetris-opt14-strips", "tidybot-opt11-strips", "tidybot-opt14-strips", "tpp", "transport-opt08-strips", "transport-opt11-strips", "transport-opt14-strips", "trucks-strips", "visitall-opt11-strips", "visitall-opt14-strips", "woodworking-opt08-strips", "woodworking-opt11-strips", "zenotravel"]
 
 if REMOTE:
     ENV = BaselSlurmEnvironment(email="maria.desteffani@unibas.ch", partition="infai_2")
+    strips_tasks = optimal_strips
 else:
     ENV = LocalEnvironment(processes=2)
+    strips_tasks = ["blocks"]
     
 ATTRIBUTES = [
     "solved",
@@ -48,19 +53,6 @@ ATTRIBUTES = [
     "error",
     "solver_exit_code",
 ]
-
-'''ALGORITHMS = {
-    "baseline_int": ["int"],
-    "baseline_set": ["set"],
-    "int_zero" : ["int", "-zh"],
-    "set_zero" : ["set", "-zh"],
-    "int_goal" : ["int" , "-gh"],
-    "set_goal" : ["set", "-gh"],
-    "int_zero_goal" : ["int", "-zh", "-gh"],
-    "set_zero_goal" : ["set", "-zh", "-gh"],
-    "int_zero_goal_ignore" : ["int", "-zh", "-gh", "-i"],
-    "set_zero_goal_ignore" : ["set", "-zh", "-gh", "-i"]
-}'''
 
 ALGORITHMS = {
     "int_baseline" : ["int", "-zh"],
@@ -89,29 +81,18 @@ def make_parser():
         type=int, 
         file="driver.log"
     )
-    # check if time solved was printed out -> needed due to issue with didppy where program doesn't stop running after printing solution
-    # parser.add_pattern("solved", "solve time:", type=str, file="driver.log")      #doesn't work
     return parser
 
 # Create the experiment
 exp = Experiment(environment=ENV)
-'''exp.add_resource("solver", os.path.join(SCRIPT_DIR, "main.py"))
-exp.add_resource("int_mapping", os.path.join(SCRIPT_DIR, "int_mapping.py"), symlink=True)
-exp.add_resource("set_mapping", os.path.join(SCRIPT_DIR, "set_mapping.py"), symlink=True)
-exp.add_resource("third_party", os.path.join(SCRIPT_DIR, "third_party"), symlink=True)
-exp.add_resource("int_mapping_mod", os.path.join(SCRIPT_DIR, "int_mapping_mod.py"), symlink=True)
-exp.add_resource("set_mapping_mod", os.path.join(SCRIPT_DIR, "set_mapping_mod.py"), symlink=True)'''
 exp.add_resource("solver", REPO_DIR/"src")
 
 exp.add_parser(make_parser())
 
-# all optimal_strips benchmarks
-#optimal_strips = ["agricola-opt18-strips", "airport", "barman-opt11-strips", "barman-opt14-strips", "blocks", "childsnack-opt14-strips", "data-network-opt18-strips", "depot", "driverlog", "elevators-opt08-strips", "elevators-opt11-strips", "floortile-opt11-strips", "floortile-opt14-strips", "freecell", "ged-opt14-strips", "grid", "gripper", "hiking-opt14-strips", "logistics00", "logistics98", "miconic", "movie", "mprime", "mystery", "nomystery-opt11-strips", "openstacks-opt08-strips", "openstacks-opt11-strips", "openstacks-opt14-strips", "openstacks-strips", "organic-synthesis-opt18-strips", "organic-synthesis-split-opt18-strips", "parcprinter-08-strips", "parcprinter-opt11-strips", "parking-opt11-strips", "parking-opt14-strips", "pathways", "pegsol-08-strips", "pegsol-opt11-strips", "petri-net-alignment-opt18-strips", "pipesworld-notankage", "pipesworld-tankage", "psr-small", "quantum-layout-opt23-strips", "rovers", "satellite", "scanalyzer-08-strips", "scanalyzer-opt11-strips", "snake-opt18-strips", "sokoban-opt08-strips", "sokoban-opt11-strips", "spider-opt18-strips", "storage", "termes-opt18-strips", "tetris-opt14-strips", "tidybot-opt11-strips", "tidybot-opt14-strips", "tpp", "transport-opt08-strips", "transport-opt11-strips", "transport-opt14-strips", "trucks-strips", "visitall-opt11-strips", "visitall-opt14-strips", "woodworking-opt08-strips", "woodworking-opt11-strips", "zenotravel"]
-strips_tasks = ["blocks"]
 benchmarks = suites.build_suite(BENCHMARKS_DIR, strips_tasks)
 
 for algo, options in ALGORITHMS.items():
-    for benchmark in benchmarks[:2]:        # CHANGE BENCHMARKS HERE: currently only the first 5 blocks tasks run
+    for benchmark in benchmarks:        
         domain_file = benchmark.domain_file
         problem_file = benchmark.problem_file
         
